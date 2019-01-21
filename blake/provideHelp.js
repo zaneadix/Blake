cc783186fd23f901379f9bfe305e750190a451d8
Zane Adickes <zaneadix@gmail.com>
2019-01-21 13:24:44 -0500

    doodoo

--- blake/provideHelp.js
+++ blake/provideHelp.js
@@ -1,3 +1,16 @@
-const provideHelp = async message => {};
+const client = require('./client');
+
+const commands = {
+  help: {
+    format: `@${client.user.username} help [command]`,
+    message: [``].join('\n')
+  },
+  log: {},
+  activity: {}
+};
+
+const provideHelp = async message => {
+  message.channel.send();
+};
 
 module.exports = provideHelp;

d497c4c3defa76a639972250763d6d5197f953f0
Zane Adickes <zane@zanes-mbp.myfiosgateway.com>
2021-01-03 21:13:47 -0500

    autoreconnect

--- blake/client.js
+++ blake/client.js
@@ -1,11 +1,11 @@
 const Discord = require("discord.js");
 const authToken = process.env.DISCORD_TOKEN;
 
-const client = new Discord.Client();
+const client = new Discord.Client({ autoReconnect: true });
 
 client
   .login(authToken)
   .then(() => {})
-  .catch(error => console.error(error));
+  .catch((error) => console.error(error));
 
 module.exports = client;
